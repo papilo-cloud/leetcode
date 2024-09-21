d0b4df8a352d7f809c010c75a1952f66c51e0886
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-21 23:38:50 +0100

    #560: Subarray Sum Equals K

---
+++ Prefix_Sum/subarray_sum.py
@@ -0,0 +1,34 @@
+# Given an array of integers nums and an integer k, return the total number
+# of subarrays whose sum equals to k.
+
+# A subarray is a contiguous non-empty sequence of elements within an array.
+
+# Example 1:
+
+# Input: nums = [1,1,1], k = 2
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,2,3], k = 3
+# Output: 2
+ 
+# Constraints:
+
+# 1 <= nums.length <= 2 * 104
+# -1000 <= nums[i] <= 1000
+# -107 <= k <= 107
+
+
+from collections import defaultdict
+class Solution:
+    def subarraySum(self, nums: list[int], k: int) -> int:
+        dictt = defaultdict(int)
+        count = prefix_sum = 0
+
+        dictt[0] = 1
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            x = prefix_sum - k
+            count += dictt[x]
+            dictt[prefix_sum] += 1
+        return count
