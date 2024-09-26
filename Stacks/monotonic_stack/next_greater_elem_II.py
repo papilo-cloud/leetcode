4ca5b686aaeabdf97ad65a415d68d60a53e7549a
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-26 21:40:31 +0100

    #503: Next Greater Element II

---
+++ Stacks/monotonic_stack/next_greater_elem_II.py
@@ -0,0 +1,40 @@
+# Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]),
+# return the next greater number for every element in nums.
+
+# The next greater number of a number x is the first greater number to its traversing-order next 
+# in the array, which means you could search circularly to find its next greater number. If it 
+# doesn't exist, return -1 for this number.
+
+
+# Example 1:
+# Input: nums = [1,2,1]
+# Output: [2,-1,2]
+# Explanation: The first 1's next greater number is 2; 
+# The number 2 can't find next greater number. 
+# The second 1's next greater number needs to search circularly, which is also 2.
+
+# Example 2:
+# Input: nums = [1,2,3,4,3]
+# Output: [2,3,4,-1,4]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+
+
+class Solution:
+    def nextGreaterElements(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        nge = [-1]*n
+        stk = []
+
+        for i in range(2*n-1, -1, -1):
+            while stk and stk[-1] <= nums[i%n]:
+                stk.pop()
+            if i < n and stk:
+                nge[i] = stk[-1]
+            stk.append(nums[i%n])
+
+        return nge
