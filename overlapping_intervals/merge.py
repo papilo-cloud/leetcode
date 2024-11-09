0fadfff8c1f9d288ee713ea1f2ece571287eb707
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-10 00:03:57 +0100

    #56. Merge Intervals

---
+++ overlapping_intervals/merge.py
@@ -0,0 +1,39 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping
+# intervals, and return an array of the non-overlapping intervals that cover all the
+# intervals in the input.
+
+ 
+
+# Example 1:
+
+# Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+# Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+# Example 2:
+
+# Input: intervals = [[1,4],[4,5]]
+# Output: [[1,5]]
+# Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+# Constraints:
+
+# 1 <= intervals.length <= 104
+# intervals[i].length == 2
+# 0 <= starti <= endi <= 104
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]: # type: ignore
+        intervals.sort()
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            last = res[-1]
+            curr = intervals[i]
+
+            if curr[0] <= last[1]:
+                last[1] = max(last[1], curr[1])
+            else:
+                res.append(curr)
+        return res
