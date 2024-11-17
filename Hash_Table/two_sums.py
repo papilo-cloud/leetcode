dd4da13fed23a98a0c085f67cfd9c07d5d6ff0d4
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-17 21:35:56 +0100

    #1. Two Sum

--- Hash_Table/two_sums.py
+++ Hash_Table/two_sums.py
@@ -30,7 +30,9 @@
 
 class Solution:
     def twoSum(self, nums: int, target: int) -> int:
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        hash_tbl = {}
+        for i, n in enumerate(nums):
+            c = target - n
+            if c in hash_tbl:
+                return [hash_tbl[c], i]
+            hash_tbl[n] = i
