cf0e236a2f6784c8a02db9db747569656a6d5116
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-09 23:23:32 +0100

    #34. Find First and Last Position of Element in Sorted Array

---
+++ Modified_binary_search/search_range.py
@@ -0,0 +1,42 @@
+# Given an array of integers nums sorted in non-decreasing order, find the 
+# starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# Example 3:
+
+# Input: nums = [], target = 0
+# Output: [-1,-1]
+ 
+
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# nums is a non-decreasing array.
+# -109 <= target <= 109
+
+
+from typing import List
+from bisect import bisect_left
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        l = bisect_left(nums, target)
+        r = bisect_left(nums, target + 1)
+
+        if l == r:
+            return -1, -1
+        else:
+            return l, r-1
